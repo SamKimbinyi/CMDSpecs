8add5be59eb640ff2783cd0bc637bfc8a07bc605
SamKimbinyi <Skwix@hotmail.co.uk>
2015-08-02 21:02:33 +0100

    -Cleaned Up Code -Added Hardware Class -Added Command Line ARguments

---
+++ CMDSpecs/Hardware.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMDSpecs
+{
+    public class Hardware
+    {
+
+        public static int bytesToGig = 1000000000;
+        public static ManagementObjectSearcher Searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Processor");
+
+
+        //  Print CPU Data
+        public static void showCPU()
+        {
+
+            Console.WriteLine(" \n >>>>CPU<<<< \n");
+            foreach (ManagementObject processor in Searcher.Get())
+            {
+
+
+                // Manufacturer
+                if (processor["manufacturer"] != null)
+                    Console.WriteLine("Manufacturer: " + processor["manufacturer"].ToString());
+                //Name
+                if (processor["name"] != null)
+                    Console.WriteLine("Name: " + processor["name"].ToString());
+
+                //Data Width
+                if (processor["datawidth"] != null)
+                    Console.WriteLine("Data Width: " + processor["datawidth"].ToString() + "bit");
+
+                //Clock Speed
+                if (processor["maxclockspeed"] != null)
+                    Console.WriteLine("Clock Speed: " + processor["maxclockspeed"].ToString() + " Mhz");
+
+                //Physical Cores
+                if (processor["numberofcores"] != null)
+                    Console.WriteLine("Physical Cores: " + processor["numberofcores"].ToString());
+
+                Console.WriteLine("Core Count: " + Environment.ProcessorCount);
+
+
+
+
+            }
+
+        }
+        
+        //Get RAM
+        public static void showRam()
+        {
+            
+            Console.WriteLine(" \n >>>>RAM<<<< \n");
+            Console.WriteLine("Capacity: " + (long)new Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory / bytesToGig + "GB");
+        }
+
+
+
+
+        //Get GPU 
+        public static void showGPU()
+        {
+
+            Console.WriteLine(" \n >>>>GPU<<<< \n");
+
+            foreach (ManagementObject mo in Searcher.Get())
+            {
+                foreach (PropertyData gpu in mo.Properties)
+                {
+                    if (gpu.Name == "Description")
+                    {
+                        Console.WriteLine("Graphics Processing Unit: " + gpu.Value.ToString());
+                    }
+                }
+
+
+            }
+        }
+
+        //Show All Specs
+        public static void showAll()
+        {
+            showCPU();
+            showGPU();
+            showRam();
+
+        }
+    }
+}
--- CMDSpecs/Program.cs
+++ CMDSpecs/Program.cs
@@ -1,68 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Management;
+
 namespace CMDSpecs
 {
     class Program
     {
 
-        public static int bytesToGig = 1000000000;
+       
+        List<Action> methods = new List<Action>();
+        public static String command;
         static void Main(string[] args)
         {
 
-            if (args.Length == 0)
+            try
             {
-                ManagementObjectSearcher Searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Processor");
-
-
-                //  Print CPU Data
-
-                Console.WriteLine(" \n >>>>CPU<<<< \n");
-                foreach (ManagementObject processor in Searcher.Get())
-                {
-
-
-                    // Manufacturer
-                    if (processor["manufacturer"] != null)
-                        Console.WriteLine("Manufacturer: " + processor["manufacturer"].ToString());
-                    //Name
-                    if (processor["name"] != null)
-                        Console.WriteLine("Name: " + processor["name"].ToString());
-
-                    //Data Width
-                    if (processor["datawidth"] != null)
-                        Console.WriteLine("Data Width: " + processor["datawidth"].ToString() + "bit");
-
-                    //Clock Speed
-                    if (processor["maxclockspeed"] != null)
-                        Console.WriteLine("Clock Speed: " + processor["maxclockspeed"].ToString() + " Mhz");
-
-
-
-
-                }
-                //Get RAM
-                Console.WriteLine(" \n >>>>RAM<<<< \n");
-                Console.WriteLine("Capacity: " + (long)new Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory / bytesToGig + "GB");
-
-
-
-                //Get GPU 
-
-                Console.WriteLine(" \n >>>>GPU<<<< \n");
-
-                foreach (ManagementObject mo in Searcher.Get()) {
-                    foreach (PropertyData gpu in mo.Properties) {
-                        if (gpu.Name == "Description") {
-                            Console.WriteLine("Graphics Processing Unit: " + gpu.Value.ToString());
-                        }
-                    }
-
-
-                }
+                command = (string)args[0];
             }
+            catch (System.IndexOutOfRangeException e) {
+                command = "";
+            }
+          
+
+            switch (command.ToLower()) {
+
+                case "cpu":
+                    Hardware.showCPU();
+                    break;
+
+                case "gpu":
+                    Hardware.showGPU();
+                    break;
+
+                case "ram":
+                    Hardware.showRam();
+                    break;
+
+
+                default:
+                    Hardware.showAll();
+                    break;
+            }
+
+#if DEBUG
+            Console.ReadLine(); //Stops Closing Automatically
+#endif
+
+
+
+
+
+
+
+
 
 
 
         }
+
+
+
     }
-}
+    }
+
